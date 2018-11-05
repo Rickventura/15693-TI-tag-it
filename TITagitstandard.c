5d27284e4e54582f491e46943ff556be3ff8b270
Rickventura <43839047+Rickventura@users.noreply.github.com>
2018-11-05 07:44:39 +0100

    Update TITagitstandard.c

--- TITagitstandard.c
+++ TITagitstandard.c
@@ -97,15 +97,15 @@ uint16_t TITagitstandardAppProcess(uint8_t* FrameBuf, uint16_t FrameBytes)
      	
 	
                       if ((FrameBuf[0] & ISO15693_REQ_FLAG_ADDRESS) && ISO15693CompareUid(&FrameBuf[2], Uid) )
-			  PageAddress = FrameBuf[10];
+			  PageAddress = FrameBuf[10]; // addressed request
 		      else
-			  PageAddress = FrameBuf[2];
+			  PageAddress = FrameBuf[2]; // unaddressed request
 			  				      
  		
                       if (FrameBuf[0] & ISO15693_REQ_FLAG_OPTION)
                           {
                               FrameBuf[0] = 0x00; /* Flags */	                      
-                              FrameBuf[1] = ( PageAddress == 8 || PageAddress == 9) ? 0x02 : 0x00; /* security dummy to 0 */	                      
+                              FrameBuf[1] = ( PageAddress == 8 || PageAddress == 9) ? 0x02 : 0x00; /* 0x02 shows factory locked pages 8,9 */	                      
 			      FramePtr = FrameBuf + 2;				      
                               ResponseByteCount = 6;
 
